f9de114d8d054741a7bda0ab2a31e091388f5de2
aelssait <holses29@gmail.com>
2026-06-12 23:25:36 +0300

    server.js

--- server.js
+++ server.js
@@ -6,20 +6,17 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-// Хранилище команд в оперативной памяти сервера
 let teamsData = []; 
 
-// Отдать данные всем пользователям
 app.get('/get-teams', (req, res) => {
     res.json(teamsData);
 });
 
-// Принять новые данные при добавлении/удалении
 app.post('/save-teams', (req, res) => {
     teamsData = req.body;
-    res.json({ success: true, message: "Данные обновлены у всех!" });
+    res.json({ success: true });
 });
 
 app.listen(PORT, () => {
-    console.log("Сервер работает на порту" , ${PORT});
+    console.log("Server is running on port", PORT);
 });
